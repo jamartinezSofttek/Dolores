d41b56e8eb5cebc541eff4e65a042ccdfa6c4d0d
jamartinezSofttek <ja.martinez@softtek.com>
2025-06-17 11:44:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ scene.add(new THREE.AmbientLight(0x404040));
 // 3. Texto 3D
 const loader = new THREE.FontLoader();
 loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', (font) => {
-    const textGeometry = new THREE.TextGeometry('De todas las Rosas tu eres la mas hermosa', {
+    const textGeometry = new THREE.TextGeometry('Dolores', {
         font: font,
         size: 0.5,
         height: 0.2,
